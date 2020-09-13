78fb55f6c36ce6621d0e3fc93acfe36547e77ae8
XBigBear <736497373@qq.com>
2020-09-13 23:24:02 +0800

    1.添加目录 2.添加utils工具

---
+++ lib/module/utils/appNavUtils/AppNavUtils.dart
@@ -0,0 +1,43 @@
+import 'package:diary_account_book/module/utils/colorUtils/ColorsUtils.dart';
+import 'package:flutter/material.dart';
+
+// ignore: must_be_immutable
+class AppNavUtil extends StatefulWidget {
+  final String title;
+  final Widget body;
+  final bool isBack;
+  List<Widget> actions = [];
+
+  AppNavUtil({Key key, this.title, this.body, this.isBack, this.actions})
+      : super(key: key);
+  _AppNavUtilState createState() => _AppNavUtilState();
+}
+
+class _AppNavUtilState extends State<AppNavUtil> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        leading: widget.isBack
+            ? BackButton(
+                color: Colors.black,
+                onPressed: () => (Navigator.pop(context)),
+              )
+            : null,
+        actions: widget.actions,
+        title: Text(widget.title,
+            style: new TextStyle(
+              color: ColorsUtil.hexColor(0x333333),
+              fontSize: 17,
+            )),
+        elevation: 0.0,
+        backgroundColor: ColorsUtil.hexColor(0xffffff),
+      ),
+      body: Container(
+        color: ColorsUtil.hexColor(0xF1F2F4),
+        child: widget.body,
+      ),
+    ));
+  }
+}
