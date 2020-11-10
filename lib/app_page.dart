4bf522ebae0a8a2f5c682a9f7e68e5b74c79c44e
XBigBear <736497373@qq.com>
2020-11-10 11:38:54 +0800

    1.MaterialApp增加navigatorKey 配合路由跳转

--- lib/app_page.dart
+++ lib/app_page.dart
@@ -1,11 +1,14 @@
+import 'package:diary_account_book/module/page/root/root_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/screenutil.dart';
 import 'module/page/login/login_home_page.dart';
+import 'module/utils/routerNavUtils/router_nav_utils.dart';
 
 class AppPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      navigatorKey: RouterNavigatorUtils.navigatorKey,
       home: _AppPageState(),
     );
   }
